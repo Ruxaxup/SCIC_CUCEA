4f63685a57b9d3b76f300787efecd87d392c31be
Gustavo <Ruxaxup@MacBook-Air-de-Gustavo.local>
2016-05-10 13:30:08 -0500

    Dictionary updated

--- sendDataMQTT.py
+++ sendDataMQTT.py
@@ -7,7 +7,19 @@ from socket import error as socket_error
 
 host = "148.202.23.200"
 canal = "sensors/CICI/boards"
-lecturas = {"temp":"temperature","press":"pressure","light":"light","noise":"noise"}
+lecturas = {"temp":"temperature",
+			"press":"pressure",
+			"light":"light",
+			"noise":"noise",
+			"water":"water",
+			"NO2":"NO2",
+			"NH3":"NH3",
+			"C3H8":"C3H8",
+			"C4H10":"C4H10",
+			"CH4":"CH4",
+			"H2":"H2",
+			"C2H5OH":"C2H5OH",
+			"hum":"humidity"}
 deli = ';'
 if len(sys.argv)  == 2:
 	tipoLectura = lecturas[sys.argv[1]]
